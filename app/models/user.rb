b2122d78c5883d3fcc2daaa9211c860e8bf34618
iamShah <formal>
2021-01-05 13:59:47 -0500

    first commit of lecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User <ActiveRecord::Base  
     has_many :orders 
 end
+
+
+# echo "# SINATRA-PROJECT-" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin git@github.com:shah2889/SINATRA-PROJECT-.git
+# git push -u origin main
