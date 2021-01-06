4695306c5d0eaab31287261b2b5c7f85a8198667
iamShah <formal>
2021-01-06 08:28:53 -0500

    still working on

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User <ActiveRecord::Base
 end
 
 
-# echo "# SINATRA-PROJECT-" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin git@github.com:shah2889/SINATRA-PROJECT-.git
-# git push -u origin main
+
+echo "# Sinatra-Project-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:shah2889/Sinatra-Project-.git
+git push -u origin main
